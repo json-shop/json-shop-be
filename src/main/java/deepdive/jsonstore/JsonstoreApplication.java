0567f42da1d525a9ff78e0af85a1ce4859eb23b1
JiELee1 <dlwldmscjstk@naver.com>
2025-04-02 15:57:39 +0900

    init

---
+++ src/main/java/deepdive/jsonstore/JsonstoreApplication.java
@@ -0,0 +1,13 @@
+package deepdive.jsonstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/deepdive/jsonstore/JsonstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package deepdive.jsonstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
