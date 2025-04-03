8b2870a29629eeafa3600ad4d7488c461869cda3
cupokki <nd1346@gmail.com>
2025-04-03 15:34:55 +0900

    feat: Order, OrderProduct 엔티티

---
+++ src/main/java/deepdive/jsonstore/domain/order/model/Order.java
@@ -0,0 +1,59 @@
+package deepdive.jsonstore.domain.order.model;
+
+import deepdive.jsonstore.common.entity.BaseEntity;
+import deepdive.jsonstore.domain.member.model.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Order extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, columnDefinition = "CHAR(36)", nullable = false)
+    private UUID uuid;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column
+    private BigDecimal total;
+
+    @Column
+    private String zipCode;
+
+    @Column
+    private String address;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @Column
+    private OrderStatus orderStatus = OrderStatus.ORDERED;
+
+    @Column
+    private String phone; // 수령인 번호
+
+    @Column
+    private String recipient; // 수령인
+
+    @Builder.Default
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderProduct> products= new ArrayList<>();
+
+}
---
+++ src/main/java/deepdive/jsonstore/domain/order/model/OrderStatus.java
@@ -0,0 +1,5 @@
+package deepdive.jsonstore.domain.order.model;
+
+public enum OrderStatus {
+    ORDERED,IN_DELIVERY,DONE,CANCELED
+}
