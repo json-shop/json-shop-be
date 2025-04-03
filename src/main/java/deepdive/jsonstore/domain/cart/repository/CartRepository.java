30f6c829f7d1b2d225534835047e62a88c1968ff
UncleSamsun <alswns5620@naver.com>
2025-04-04 00:02:16 +0900

    feat: 장바구니, 알람 관련 엔티티 및 리파지토리 추가

---
+++ src/main/java/deepdive/jsonstore/domain/cart/repository/CartRepository.java
@@ -0,0 +1,15 @@
+package deepdive.jsonstore.domain.cart.repository;
+
+import deepdive.jsonstore.domain.cart.model.Cart;
+import deepdive.jsonstore.domain.member.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    // 특정 회원의 장바구니 조회 (회원당 하나만 존재)
+    Optional<Cart> findByMember(Member member);
+}
