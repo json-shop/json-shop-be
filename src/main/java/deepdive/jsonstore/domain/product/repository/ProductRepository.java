4069326a71cd2d9e22bb7975dc8cb7fffc887c41
JiELee1 <dlwldmscjstk@naver.com>
2025-04-03 20:44:53 +0900

    [FEAT] 상품 엔티티 및 리포지토리, 검증 서비스 추가

---
+++ src/main/java/deepdive/jsonstore/domain/product/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package deepdive.jsonstore.domain.product.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import deepdive.jsonstore.domain.product.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	Optional<Product> findByUuidAndActiveIsTrue(UUID uuid);
+}
