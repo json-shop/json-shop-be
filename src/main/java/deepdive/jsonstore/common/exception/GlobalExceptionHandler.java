1219616c02309557bcb61f285161f2d261801323
jeonghwan123 <dhshin444@naver.com>
2025-04-04 15:58:20 +0900

    수정 내용 저장

--- src/main/java/deepdive/jsonstore/common/exception/GlobalExceptionHandler.java
+++ src/main/java/deepdive/jsonstore/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,7 @@
 package deepdive.jsonstore.common.exception;
 
 import deepdive.jsonstore.common.dto.ErrorDto;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,13 +14,5 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(e.getHttpStatus())
                 .body(errorDto);
     }
-    // @Valid 검증 실패 예외 처리 추가
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorDto> handleValidationException(MethodArgumentNotValidException e) {
-        String errorMessage = e.getBindingResult().getAllErrors().getFirst().getDefaultMessage();
-        var errorDto = new ErrorDto(HttpStatus.UNPROCESSABLE_ENTITY, errorMessage);
-        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(errorDto);
-    }
-
 
 }
