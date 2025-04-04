b900e9d24979b09d8b2b098b989e272ab4ac3b14
JiELee1 <dlwldmscjstk@naver.com>
2025-04-04 18:43:10 +0900

    [ADD] 임시 상품 등록 추가

--- src/main/java/deepdive/jsonstore/domain/product/controller/ProductController.java
+++ src/main/java/deepdive/jsonstore/domain/product/controller/ProductController.java
@@ -1,14 +1,18 @@
 package deepdive.jsonstore.domain.product.controller;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import deepdive.jsonstore.domain.product.dto.ActiveProductResponse;
+import deepdive.jsonstore.domain.product.dto.ProductRequest;
 import deepdive.jsonstore.domain.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +28,12 @@ public class ProductController {
 	public ResponseEntity<ActiveProductResponse> getActiveProduct(@PathVariable UUID productId) {
 		ActiveProductResponse res = productService.getActiveProductDetail(productId);
 		return ResponseEntity.ok(res);
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> saveTemp(@RequestBody List<ProductRequest> productRequestList) {
+		productService.tempSave(productRequestList);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/deepdive/jsonstore/domain/product/service/ProductService.java
+++ src/main/java/deepdive/jsonstore/domain/product/service/ProductService.java
@@ -1,25 +1,33 @@
 package deepdive.jsonstore.domain.product.service;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import deepdive.jsonstore.domain.product.dto.ActiveProductResponse;
+import deepdive.jsonstore.domain.product.dto.ProductRequest;
 import deepdive.jsonstore.domain.product.entity.Product;
+import deepdive.jsonstore.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class ProductService {
 
 	private final ProductValidationService productValidationService;
+	private final ProductRepository productRepository;
 
+	@Transactional(readOnly = true)
 	public ActiveProductResponse getActiveProductDetail(UUID id) {
 		Product product = productValidationService.findActiveProductById(id);
-		return product.toActiveProductResponse();
+		return ActiveProductResponse.toActiveProductResponse(product);
+	}
+
+	public void tempSave(List<ProductRequest> productRequestList) {
+		productRequestList.forEach(p -> productRepository.save(p.toProduct("test.jpg")));
 	}
 }
