7de8192305f8f1f5e5b2f90968db405747728ba4
cupokki <nd1346@gmail.com>
2025-04-09 15:03:17 +0900

    feat : 비관적락

--- src/main/java/deepdive/jsonstore/domain/product/repository/ProductRepository.java
+++ src/main/java/deepdive/jsonstore/domain/product/repository/ProductRepository.java
@@ -3,13 +3,21 @@ package deepdive.jsonstore.domain.product.repository;
 import java.util.Optional;
 import java.util.UUID;
 
+import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import deepdive.jsonstore.domain.product.entity.Product;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	Optional<Product> findByUidAndActiveIsTrue(UUID uuid);
 
 	Optional<Product> findByUid(UUID productUid);
+
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@Query("SELECT p FROM Product p WHERE p.id = :id")
+	Optional<Product> findWithLockById(@Param("id") Long id);
 }
