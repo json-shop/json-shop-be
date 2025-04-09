c441e54950dd7869adf138272a3748f66eee2168
cupokki <nd1346@gmail.com>
2025-04-09 15:58:06 +0900

    feat : 컨펌, 웹훅용 임시 DTO

---
+++ src/main/java/deepdive/jsonstore/domain/order/dto/ConfirmRequest.java
@@ -0,0 +1,24 @@
+package deepdive.jsonstore.domain.order.dto;
+
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+//public record ConfirmRequest (
+//     String type, //Transaction.Confirm 고정
+//     LocalDateTime timestamp,
+//     ConfirmDataRequest data
+//){
+//
+//}
+
+@Builder
+public record ConfirmRequest (
+        String imp_uid,
+        String merchant_uid,
+        int amount
+){
+
+}
+
