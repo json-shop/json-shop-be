e5a27ccebfb6a6c0079e0beea2aee09deb1ad126
JiELee1 <dlwldmscjstk@naver.com>
2025-04-11 20:14:32 +0900

    feat: 상품 전체 조회 기능

--- src/main/java/deepdive/jsonstore/domain/product/repository/ProductRepository.java
+++ src/main/java/deepdive/jsonstore/domain/product/repository/ProductRepository.java
@@ -1,8 +1,10 @@
 package deepdive.jsonstore.domain.product.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import deepdive.jsonstore.domain.product.dto.ProductOrderCountDTO;
 import io.lettuce.core.dynamic.annotation.Param;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -26,4 +28,9 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	@Query("SELECT p FROM Product p WHERE p.id = :id")
 	Optional<Product> findWithLockById(@Param("id") Long id);
+
+	@Query("SELECT new deepdive.jsonstore.domain.product.dto.ProductOrderCountDTO(p, COALESCE(SUM(op.quantity), 0)) " +
+		"FROM Product p LEFT JOIN OrderProduct op ON p.id = op.product.id " +
+		"GROUP BY p.id")
+	List<ProductOrderCountDTO> findSoldCount();
 }
