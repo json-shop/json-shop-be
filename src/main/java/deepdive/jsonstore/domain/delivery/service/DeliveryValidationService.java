8fc4d55bf4c53cef46416ab4beae06e2de9a472c
leeingu <xltmsk@naver.com>
2025-04-12 00:10:22 +0900

    refactor: 피드백 사항 적용, 주소 인증하는 기능 분리

--- src/main/java/deepdive/jsonstore/domain/delivery/service/DeliveryValidationService.java
+++ src/main/java/deepdive/jsonstore/domain/delivery/service/DeliveryValidationService.java
@@ -1,58 +1,30 @@
 package deepdive.jsonstore.domain.delivery.service;
 
+import deepdive.jsonstore.domain.delivery.entity.Delivery;
 import deepdive.jsonstore.domain.delivery.exception.DeliveryException;
+import deepdive.jsonstore.domain.delivery.repository.DeliveryRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.UUID;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class DeliveryValidationService {
 
-    private final RestTemplate restTemplate;
+    private final DeliveryRepository deliveryRepository;
 
-    private static final String BASE_URL = "https://www.juso.go.kr/addrlink/addrLinkApi.do";
-
-    public DeliveryValidationService(RestTemplateBuilder builder) {
-        this.restTemplate = builder.build(); // 테스트에서 주입 가능
+    public Delivery getDeliveryByUid(UUID uid) {
+        Delivery delivery = deliveryRepository.findByUid(uid).orElseThrow(DeliveryException.DeliveryNotFoundException::new);
+        return delivery;
     }
 
-    @Value("${external.api.address.key:dummy-key}")
-    private String ADDRESS_API_KEY;
-
-    public boolean validateZipCode(String zipcode) {
-        String url = UriComponentsBuilder.fromHttpUrl(BASE_URL)
-                .queryParam("currentPage", 1)
-                .queryParam("countPerPage", 1)
-                .queryParam("keyword", zipcode)
-                .queryParam("confmKey", ADDRESS_API_KEY)
-                .queryParam("resultType", "json")
-                .toUriString();
-
-        String response = restTemplate.getForObject(url, String.class);
-
-        JSONObject json = new JSONObject(response);
-
-        JSONObject results = json.getJSONObject("results");
-        JSONObject common = results.getJSONObject("common");
-        String errorCode = common.getString("errorCode");
-        String errorMessage = common.getString("errorMessage");
-
-        //api key 또는 api 서버 문제
-        if ("E0001".equals(errorCode) || "-999".equals(errorCode)) {
-            log.warn("Address API returned error. code={}, message={}", errorCode, errorMessage);
-            throw new DeliveryException.AddressAPIException();
+    public void validateMember(Delivery delivery, UUID memberUid) {
+        if (!delivery.getMember().getUid().equals(memberUid)) {
+            throw new DeliveryException.DeliveryAccessDeniedException();
         }
-
-        JSONArray address = results.getJSONArray("juso");
-
-        return address.length() > 0;
-
     }
 
 }
