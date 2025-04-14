06a45bd8d07ef5ed3613c23057de2f9a7e85a512
jeonghwan123 <dhshin444@naver.com>
2025-04-14 17:59:49 +0900

    관리자 계정 생성 및 제거

--- src/main/java/deepdive/jsonstore/JsonstoreApplication.java
+++ src/main/java/deepdive/jsonstore/JsonstoreApplication.java
@@ -3,6 +3,7 @@ package deepdive.jsonstore;
 import deepdive.jsonstore.common.config.RedisTestService;
 import deepdive.jsonstore.domain.admin.entity.Admin;
 import deepdive.jsonstore.domain.admin.repository.AdminRepository;
+import jakarta.annotation.PreDestroy;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,23 +20,20 @@ import java.util.UUID;
 @EnableScheduling
 public class JsonstoreApplication {
 
-	private Admin createdAdmin; // 생성된 관리자 계정 정보를 저장
+	private final AdminRepository adminRepository;
+	private Admin createdAdmin;
+
+	public JsonstoreApplication(AdminRepository adminRepository) {
+		this.adminRepository = adminRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JsonstoreApplication.class, args);
 	}
 
 	@Bean
-	public CommandLineRunner testRedis(RedisTestService redisTestService) {
-		return args -> redisTestService.testRedisConnection();
-	}
-
-
-	// 테스트를 위한 관리자 계정 생성
-	@Bean
-	public CommandLineRunner insertAdminAccount(AdminRepository adminRepository, PasswordEncoder passwordEncoder) {
+	public CommandLineRunner insertAdminAccount(PasswordEncoder passwordEncoder) {
 		return args -> {
-			// 관리자 계정 생성
 			Admin admin = Admin.builder()
 					.uid(UUID.randomUUID())
 					.username("admin1")
@@ -45,28 +43,16 @@ public class JsonstoreApplication {
 					.deleted(false)
 					.build();
 
-			createdAdmin = adminRepository.save(admin); // 생성된 관리자 계정 저장
+			createdAdmin = adminRepository.save(admin);
 			System.out.println("관리자 계정 생성: " + admin.getEmail());
 		};
 	}
-	@Bean
-	public CommandLineRunner deleteAdminOnShutdown(AdminRepository adminRepository) {
-		return args -> {
-			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-				try {
-					// 애플리케이션 컨텍스트가 활성 상태인지 확인
-					if (adminRepository != null) {
-						adminRepository.delete(createdAdmin);
-						System.out.println("관리자 계정 삭제: " + createdAdmin.getEmail());
-					}
-				} catch (IllegalStateException e) {
-					// 컨텍스트가 종료된 상태에서 접근 시 예외 처리
-					System.err.println("애플리케이션 컨텍스트가 종료된 후 작업 시도. 무시합니다.");
-				}
-			}));
-		};
+
+	@PreDestroy
+	public void cleanup() {
+		if (createdAdmin != null) {
+			adminRepository.delete(createdAdmin);
+			System.out.println("관리자 계정 삭제: " + createdAdmin.getEmail());
+		}
 	}
-
-
-	}
-
+}
