b24711273b45299dd1a18a09008f767df591a4e5
jeonghwan123 <dhshin444@naver.com>
2025-04-15 11:40:09 +0900

    fix: SecurityConfig 수정

--- src/main/java/deepdive/jsonstore/common/config/SecurityConfig.java
+++ src/main/java/deepdive/jsonstore/common/config/SecurityConfig.java
@@ -95,7 +95,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/login", "/api/v1/admin/login", "/api/v1/join","/api/v1/admin/join").permitAll()
 
                         // 관리자 전용 경로
-                        .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/admin/**").hasAuthority("ADMIN")
 
                         // 멤버 전용 경로
                         .requestMatchers("/api/v1/member/**").hasAuthority("MEMBER")
